47aec82006355ca4d834da1c65b8a0f39b248fd1
sruthijohnson <sruthijohnson791@gmail.com>
2024-12-19 21:25:47 -0600

    Change in background color (feature1)

--- App.js
+++ App.js
@@ -39,7 +39,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: 'pink',
+    backgroundColor: 'grey',
     padding: 20,
   },
   card: {
